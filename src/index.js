4f91f816de5acc295f0c98a233f06f079008a5c6
Paola Fantinel <fantinelpaola15@gmail.com>
2023-07-12 18:11:43 -0300

    Modificação index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 // import 'w3-css/w3.css'
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
